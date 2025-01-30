1557538036c1671443f8f48bdd658250420abca8
Hamza Rajpoot <hamza.rajpoot@appinsnap.com>
2025-01-30 18:22:47 +0500

    logger enhancing

--- src/test/java/com/point/of/sale/pos/POSServiceTest.java
+++ src/test/java/com/point/of/sale/pos/POSServiceTest.java
@@ -15,16 +15,16 @@ public class POSServiceTest {
     public void processTransaction() {
         POSService posService = new POSService();
 
-        Item item1 = new Item("Wallet", 20.0, 1); //wallet price is 20rs
-        Item item2 = new Item("Belt", 40.0, 1); //belt price is 40rs
+        Item item1 = new Item("Wallet", 20.0, 1); //in this case wallet price is 20
+        Item item2 = new Item("Belt", 40.0, 1); //in this case belt price is 40
 
-        //customer provide amount against products and will be added to persistence list
+        //customer provide amount against products and will be added to persistence layer
         Transaction transaction = posService.processSale(Arrays.asList(item1, item2), 80.0);
 
         //calculating total amount of products
-        assertEquals(60.0, transaction.processTotalAmount(), "total amount of products" + "(" + item1.getName() + item2.getName() + ")" + "should be 60.0");
+        assertEquals(60.0, transaction.processTotalAmount(), "total amount of products " + "(" + item1.getName() + "," + item2.getName() + ")" + " should be " + transaction.getTotalAmount());
 
         //calculate change need to give back to customer
-        assertEquals(20.0, transaction.processBalanceAfterTransaction(), "the amount that will give back to customer should be 20.0");
+        assertEquals(20.0, transaction.processBalanceAfterTransaction(), "the amount that will give back to customer should be " + transaction.getChange());
     }
 }
